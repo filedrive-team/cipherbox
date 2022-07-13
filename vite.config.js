d89f944df4b76a5dc969b5293e616813ada04c3e
abu0306 <2261983469@qq.com>
2022-07-13 21:30:10 +0800

    feat: add the basic configuration for the project

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
-import { resolve, join } from 'path';
-// https://vitejs.dev/config/
+import { join } from 'path';
+import requireTransform from 'vite-plugin-require-transform';
+import svgr from 'vite-plugin-svgr';
+
 export default defineConfig({
-  plugins: [react(), eslintPlugin()],
+  plugins: [
+    react(),
+    eslintPlugin(),
+    requireTransform({
+      fileRegex: /.jsx$/,
+    }),
+    svgr({}),
+  ],
   resolve: {
     alias: {
       '@': join(__dirname, 'src'),
