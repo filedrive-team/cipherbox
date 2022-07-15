1864c3697262617010cc96a35c0e065ccd44670c
abu0306 <2261983469@qq.com>
2022-07-15 17:32:40 +0800

    perf: new global style

--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -1,5 +1,19 @@
 const Test = () => {
-  return <div>测试</div>;
+  return (
+    <div
+      onClick={() => {
+        window.history.back();
+      }}
+      style={{
+        background: 'yellow',
+        position: 'absolute',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      测试
+    </div>
+  );
 };
 
 export default Test;
