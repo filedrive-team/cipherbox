e2431c97b2bf349571221f5b90375c2e3e07fa1f
beeleelee <beeleelee@outlook.com>
2022-07-19 11:09:45 +0800

    refactor:no need for module of models

--- src-tauri/src/mgr/typs.rs
+++ src-tauri/src/mgr/typs.rs
@@ -5,9 +5,6 @@ use std::{
 };
 use serde::{Serialize, Deserialize};
 use crate::{
-    models::{
-        CBox, CBoxObj, Provider,
-    },
     errors::Error,
 };
 use rusqlite::{
@@ -73,6 +70,63 @@ impl<T: Serialize> CommonRes<T> {
     pub fn ok(d: T) -> Self {
         CommonRes { error: "".into(), result: Some(d) }
     }
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CBox {
+    pub id: i32,
+    pub name: String,
+    // most of time backup should be encrypt unless user intentionly set it false, maybe for public share
+    pub encrypt_data: bool,
+    // total objects in the box
+    pub obj_total: u64,
+    // total size of objects in the box
+    pub size_total: u64,
+    // the key use to do encrypt works
+    #[serde(skip_deserializing)]
+    pub secret: Vec<u8>,
+    // the storage provider, like web3.storage
+    pub provider: i32,
+    // access token for provider api
+    pub access_token: String,
+    // the current showing box for user
+    pub active: u8,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CBoxObj {
+    pub id: i32,
+    pub box_id: i32,
+    pub provider: i32,
+    // encrypted data cid 
+    pub cid: String,
+    #[serde(skip_deserializing)]
+    pub nonce: Vec<u8>,
+    pub size: u64,
+    // filename
+    pub name: String,
+    // relative path
+    pub path: String,
+    // path of file in host file system
+    pub origin_path: String,
+    // object type - 0 file | 1 directory
+    pub obj_type: u8,
+}
+
+#[derive(Debug)]
+pub struct Identity {
+    id: i32,
+    secret: Vec<u8>
+}
+
+#[derive(Debug)]
+pub struct Provider {
+    pub id: i32,
+    pub name: String,
+    pub put_api: String,
+    pub get_api: String,
 }
 
 pub fn current() -> Result<u64, SystemTimeError>{
