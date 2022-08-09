df41dc8669635adae36f6076128e506dad38406e
abu0306 <2261983469@qq.com>
2022-08-09 17:21:14 +0800

    feat: resolve merge conflict

--- src/pages/Box/index.jsx
+++ src/pages/Box/index.jsx
@@ -50,6 +50,29 @@ const columns = [
     dataIndex: 'createAt',
     key: 'createAt',
   },
+  {
+    title: '操作',
+    dataIndex: 'operate',
+    key: 'operate',
+    render: (_, value) => {
+      if (value.exists) {
+        return (
+          <div
+            onClick={async () => {
+              await new shell.Command('show-in-finder', [
+                '-R',
+                value.originPath,
+              ]).spawn();
+            }}
+          >
+            <OpenIcon />
+          </div>
+        );
+      } else {
+      }
+      return <DownLoadIcon />;
+    },
+  },
 ];
 
 const Box = () => {
