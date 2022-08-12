1e0a7dfebf2b4bb83cbb80fa1360549938ca1287
abu0306 <2261983469@qq.com>
2022-08-12 15:31:24 +0800

    feat:  adjust backup api

--- src/pages/Backup/index.jsx
+++ src/pages/Backup/index.jsx
@@ -16,6 +16,7 @@ import { ReactComponent as StopIcon } from '@/assets/backup/stop.svg';
 import { useEffect } from 'react';
 import backupStore from '@/store/modules/backup';
 import { observer } from 'mobx-react';
+import BigNumber from 'bignumber.js';
 const tabData = [
   {
     icon: copyIcon,
@@ -26,85 +27,6 @@ const tabData = [
     icon: switchIcon,
     bg: switchButton,
     name: '已备份',
-  },
-];
-
-const columns = [
-  {
-    title: 'Path',
-    dataIndex: 'originPath',
-    key: 'originPath',
-    width: 50,
-    render: (text) => {
-      return (
-        <Tooltip trigger={'click'} title={text}>
-          <div className={classNames(styles.path)}>{text}</div>
-        </Tooltip>
-      );
-    },
-  },
-  {
-    title: 'Size',
-    dataIndex: 'size',
-    key: 'size',
-    align: 'center',
-    render: (text) => <div>{'-'}</div>,
-  },
-  {
-    title: '进度',
-    dataIndex: 'createAt',
-    key: 'createAt',
-    render: (_, value) => {
-      return (
-        <div className={styles.progressWrap}>
-          <Progress
-            strokeColor={{
-              '0%': '#32fbff',
-              '30%': '#336AFA',
-              '100%': '#b199ff',
-              direction: '90deg',
-            }}
-            trailColor={'#F7F7F7'}
-            percent={value.percent}
-            showInfo={false}
-            strokeWidth={4}
-          ></Progress>
-          <div>正在备份中{value.percent}%</div>
-        </div>
-      );
-    },
-  },
-  {
-    title: '操作',
-    dataIndex: 'operation',
-    key: 'operation',
-    width: '160',
-    align: 'right',
-    render: (_, value) => {
-      return (
-        <div className={styles.operationWrap}>
-          <DeleteIcon
-            onClick={() => {
-              console.log('====DeleteIcon====++++=');
-            }}
-          />
-
-          {value.start ? (
-            <StopIcon
-              onClick={() => {
-                console.log('====StopIcon====++++=');
-              }}
-            />
-          ) : (
-            <StartIcon
-              onClick={() => {
-                console.log('====StartIcon====++++=');
-              }}
-            />
-          )}
-        </div>
-      );
-    },
   },
 ];
 
@@ -138,6 +60,92 @@ const alreadyColumns = [
 ];
 
 const Backup = () => {
+  const columns = [
+    {
+      title: 'Path',
+      dataIndex: 'originPath',
+      key: 'originPath',
+      width: 50,
+      render: (text) => {
+        return (
+          <Tooltip trigger={'click'} title={text}>
+            <div className={classNames(styles.path)}>{text}</div>
+          </Tooltip>
+        );
+      },
+    },
+    {
+      title: 'Size',
+      dataIndex: 'size',
+      key: 'size',
+      align: 'center',
+      render: (text) => <div>{'-'}</div>,
+    },
+    {
+      title: '进度',
+      dataIndex: 'createAt',
+      key: 'createAt',
+      render: (_, value) => {
+        let x = new BigNumber(value.finishedSize);
+        let y = new BigNumber(value.totalSize);
+        let p = 0;
+        if (y.eq(0) === false) {
+          p = x.dividedBy(y).times(100).toFormat(2);
+        }
+
+        return (
+          <div className={styles.progressWrap}>
+            <Progress
+              strokeColor={{
+                '0%': '#32fbff',
+                '30%': '#336AFA',
+                '100%': '#b199ff',
+                direction: '90deg',
+              }}
+              trailColor={'#F7F7F7'}
+              percent={p}
+              showInfo={false}
+              strokeWidth={4}
+            ></Progress>
+            <div>正在备份中{parseFloat(p)}%</div>
+          </div>
+        );
+      },
+    },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+      key: 'operation',
+      width: '160',
+      align: 'right',
+      render: (_, value) => {
+        return (
+          <div className={styles.operationWrap}>
+            <DeleteIcon
+              onClick={() => {
+                console.log('====DeleteIcon====++++=', backupStore.data);
+              }}
+            />
+
+            {value.start ? (
+              <StopIcon
+                onClick={() => {
+                  console.log('====StopIcon====++++=');
+                }}
+              />
+            ) : (
+              <StartIcon
+                onClick={() => {
+                  console.log('====StartIcon====++++=');
+                }}
+              />
+            )}
+          </div>
+        );
+      },
+    },
+  ];
+
   const [currentActive, setCurrentActive] = useState(0);
 
   async function task() {
--- src/store/modules/backup.js
+++ src/store/modules/backup.js
@@ -1,5 +1,7 @@
 import { invoke } from '@tauri-apps/api';
-import { makeObservable, observable, runInAction } from 'mobx';
+import { listen } from '@tauri-apps/api/event';
+import { action, makeObservable, observable, runInAction } from 'mobx';
+import _ from 'lodash';
 
 class BackupStore {
   /**
@@ -10,22 +12,25 @@ class BackupStore {
    * cid:string,
    * createAt:number,
    * hash:string,
-   * id:number
+   * id:number,
    * ,modifyAt:number,
    * name:string,
    * objType:number,
    * originPath:string,
    * path:string,
    * size:number,
-   * status:number
-   * originPath:string
+   * status:number,
+   * originPath:string,
+   * total_size:number,
+   * finished_size:number
    * }]
    *
    */
   data = [];
 
   /**
-   *@type [{start:boolean,
+   *@type [{
+   * start:boolean,
    * percent:number,
    * boxId:number,
    * cid:string,
@@ -48,22 +53,59 @@ class BackupStore {
     makeObservable(this, {
       data: observable,
       alreadyData: observable,
+      fetchData: action,
+      fetchAreadyData: action,
+      SET_CHANGE_DATA: action,
+    });
+
+    listen('task_update', (event) => {
+      console.log('+==============event=====00======', event.payload);
+      if (event.event === 'task_update') {
+        this.SET_CHANGE_DATA(event.payload).then();
+      }
+    });
+  }
+
+  /**
+   *
+   * @param {{backup:boolean,box_id:number,finished:number,finished_size:number,recover:boolean,task_id:number,total:number,total_size:number}} item
+   */
+  async SET_CHANGE_DATA(item) {
+    let _data = _.clone(this.data);
+    _data.map((value, index) => {
+      console.log('+=============000=======', value.id, item.task_id);
+      if (value.id === item.task_id) {
+        console.log('+============111========', item.task_id);
+
+        value.totalSize = item.total_size;
+        value.finishedSize = item.finished_size;
+      }
+      return value;
+    });
+
+    runInAction(() => {
+      this.data = _data;
     });
   }
 
   async fetchData() {
     const taskList = await invoke('task_list', {
-      status: [0, 1, 3, 4, 5, 6, 7, 8, 9],
+      status: [0, 1, 6, 9],
     });
     const result = taskList.result;
+
+    console.log('========fetchData=======', result);
+
     runInAction(() => {
       this.data = result;
     });
   }
 
   async fetchAreadyData() {
-    const taskList = await invoke('task_list', { status: [2] });
+    const taskList = await invoke('task_list', { status: [5] });
     const result = taskList.result;
+    console.log('========fetchAreadyData=======', result);
+
     runInAction(() => {
       this.alreadyData = result;
     });
